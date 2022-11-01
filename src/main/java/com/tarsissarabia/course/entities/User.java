de396cbf4b63ffad1291ed87dcf5fef958691b75
Tarsis Sarabia <tarsisfabiano@gmail.com>
2022-11-01 17:41:37 -0300

    JPA repository, dependency injection, database seding

--- src/main/java/com/tarsissarabia/course/entities/User.java
+++ src/main/java/com/tarsissarabia/course/entities/User.java
@@ -10,10 +10,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name= "tb_user")
+@Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
-    
+  
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
---
+++ src/main/java/com/tarsissarabia/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tarsissarabia.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tarsissarabia.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
